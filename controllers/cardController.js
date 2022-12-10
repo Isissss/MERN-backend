f2424bccdecdafc7e51680c3bdafc2b558bdd6f7
Isis <1036029@hr.nl>
2022-12-10 17:16:22 +0100

    move pagination to sep file and add more fillable fields to model

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -1,6 +1,7 @@
 const { mongoose } = require('mongoose')
+const { createPagination } = require('../helpers/pagination')
 const Card = require('../Schemas/cardModel')
-const baseURI = process.env.BASE_URI
+
 
 const cardExists = async (req, res, next) => {
 
@@ -20,64 +21,7 @@ const cardExists = async (req, res, next) => {
 
     }
 }
-const getLastPageStart = (totalCards, limit) => { return ((Math.ceil(totalCards / limit) * limit) - limit) + 1 }
 
-const getQueryString = (position, totalCards, start, limit) => {
-    if (limit == totalCards) {
-        return baseURI
-    }
-
-    switch (position) {
-        case "first":
-            startValue = 1
-            break
-        case "last":
-            startValue = getLastPageStart(totalCards, limit)
-            break
-        case "previous":
-            startValue = (start - limit < 1) ? 1 : start - limit
-            break
-        case "next":
-            startValue = (start + limit > totalCards) ? getLastPageStart(totalCards, limit) : start + limit
-            break
-        default:
-            return baseURI
-    }
-    return baseURI + `?start=${startValue}&limit=${limit}`
-}
-const getCurrentItems = (totalCards, start, limit) => { return (totalCards - start < limit) ? totalCards - start + 1 : limit }
-
-const createPagination = (totalCards, start, limit) => {
-
-    const page = Math.ceil(start / limit)
-    const totalPages = Math.ceil(totalCards / limit)
-
-    return {
-        "currentPage": page,
-        "currentItems": getCurrentItems(totalCards, start, limit),
-        "totalPages": totalPages,
-        "totalItems": totalCards,
-        "_links": {
-            "first": {
-                "page": 1,
-                "href": getQueryString("first", totalCards, start, limit)
-            },
-            "last": {
-                "page": totalPages,
-                "href": getQueryString("last", totalCards, start, limit)
-            },
-            "previous": {
-                "page": (page <= 1) ? 1 : page - 1,
-                "href": getQueryString("previous", totalCards, start, limit)
-            },
-            "next": {
-                "page": (page >= totalPages) ? totalPages : page + 1,
-                "href": getQueryString("next", totalCards, start, limit)
-            }
-        }
-
-    }
-}
 
 const getCards = async (req, res) => {
 
