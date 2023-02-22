8683aba6ca5e84afbb8a73a631bacd5f8997445a
Isis <1036029@hr.nl>
2023-02-22 19:24:11 +0100

    Push CORS

--- routes/registerController.js
+++ routes/registerController.js
@@ -4,7 +4,11 @@ const router = express.Router()
 const usersController = require('../controllers/userController')
 const verifyJWT = require('../middleware/verifyJWT')
 
-
+router.use('/', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
 router.route('/')
     .post(usersController.registerUser)
