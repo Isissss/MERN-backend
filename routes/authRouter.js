e1602ca08475b74edac409b35c9c51cf98750ce6
Isis <1036029@hr.nl>
2023-02-25 21:05:19 +0100

    Add missing headers

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,7 +5,7 @@ const authController = require('../controllers/authController')
 router.use('/', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:1234")
     res.header('Access-Control-Allow-Credentials', true)
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
