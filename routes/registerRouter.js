def8ef92bab16afa85c39194f76dba2490a1c609
Isis <1036029@hr.nl>
2023-02-25 21:32:00 +0100

    add missing header

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -5,7 +5,7 @@ const usersController = require('../controllers/userController')
 const verifyJWT = require('../middleware/verifyJWT')
 
 router.use('/', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "http://localhost:1234")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
