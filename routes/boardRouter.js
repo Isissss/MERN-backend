bb24947c8e86f3a1c96bcddf00bfaabbeb672818
Isis <1036029@hr.nl>
2023-02-25 21:05:12 +0100

    Add missing headers

--- routes/boardRouter.js
+++ routes/boardRouter.js
@@ -6,7 +6,7 @@ const verifyOwnership = require('../middleware/verifyOwnership');
 router.use('/', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:1234")
     res.header('Access-Control-Allow-Credentials', true)
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
--- routes/cardRouter.js
+++ routes/cardRouter.js
@@ -4,7 +4,7 @@ const { getCards, showCard, createCard, deleteCard, updateCard, cardsOptions, ca
 
 router.use('/', (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept")
     next()
 })
 
